498f64932fbddce72c79f744696630895ea1095c
Luis Antónimo <luisantonimo@gmail.com>
2022-07-12 08:52:40 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace T04Ejercicio11
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 10;
+            int b = 40;
+            if (a > b)
+            {
+                Console.WriteLine("El número A es mayor que B.");
+            }
+            else
+            {
+                if (a == b)
+                {
+                    Console.WriteLine("El número A es igual a B.");
+                }
+                else
+                {
+                    Console.WriteLine("El número B es mayor que A.");
+                }
+            }
+        }
+    }
+}
